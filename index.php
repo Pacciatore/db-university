986a237948ccd4603fc4881434acecebcee6edfc
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-24 19:57:09 +0200

    display corsi con rischiesta sql

--- index.php
+++ index.php
@@ -31,6 +31,9 @@ if ($conn && $conn->connect_error) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="style/style.css">
+
     <title>University</title>
 </head>
 
@@ -64,8 +67,26 @@ if ($conn && $conn->connect_error) {
 
             <h3>Corsi</h3>
 
-            <ul>
-                <li></li>
+            <ul class="list-unstyled">
+                <?php
+
+                $sql = "SELECT name, level FROM `degrees`;";
+                $result = $conn->query($sql);
+
+                if ($result && $result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                ?>
+                        <li> <?= $row['name'] . '' . $row['level'] ?> </li>
+                <?php
+                    }
+                } elseif ($result) {
+                    echo "0 results";
+                } else {
+                    echo "query error";
+                }
+
+                ?>
             </ul>
 
         </section>
